3c17e709432c5b8c393e18ca1c410d1e7e220a51
TeraWing <winfun.chan@gmail.com>
2019-09-08 17:05:15 +0800

    add LocalizationUtil

---
+++ LocalizationUtil/LocalizationUtil.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class LocalizationUtil {
+    
+    static var language: String {
+        return Locale.preferredLanguages[0]
+    }
+    
+    // key: Back, Done, Cancel ...
+    static func getSystemString(_ key: String, bundleClass: AnyClass = UIButton.self) -> String? {
+        let bundle = Bundle(for: bundleClass)
+        return bundle.localizedString(forKey: key, value: nil, table: nil)
+    }
+    
+    // code: USD -> Symbol: $
+    static func getCurrencySymbol(code: String) -> String? {
+        let locale = NSLocale(localeIdentifier: code)
+        return locale.displayName(forKey: NSLocale.Key.currencySymbol, value: code)
+    }
+}
